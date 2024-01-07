1adc5f9cb6024165400b12270a62281edf530edd
G.SO <36795023+Typse@users.noreply.github.com>
2024-01-07 16:26:53 +0100

    fertig

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if((i % 4 == 0) && (i != 0)) {
+                System.out.println(i << 2);
+            }
+        }
     }
 }
